5cbf6a71130d0fe48f2ce464265d4ee11f80c274
Iksal1592 <alsrms1592@gmail.com>
2024-08-25 12:02:23 +0900

    my first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template, url_for, redirect
+
+app = Flask(__name__, static_folder='/Users/SMK/Coding/Python/my_flask_app/static', template_folder='/Users/SMK/Coding/Python/my_flask_app/templates')
+
+users = {
+    'smk1': '1234'
+}
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            return redirect(url_for('subpage'))
+        else:
+            error = "아이디 또는 비밀번호가 올바르지 않습니다."
+    return render_template('index.html', error=error)
+
+
+
+@app.route('/subpage')
+def subpage():
+    return render_template('subpage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
