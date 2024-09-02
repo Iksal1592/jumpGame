c65be4a29fbee3069960cbfac1647ae28d30b2c0
Iksal1592 <alsrms1592@gmail.com>
2024-09-02 13:04:39 +0900

    fifth

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, url_for, redirect
 
-app = Flask(__name__, static_folder='.\\static', template_folder='.\\templates')
+app = Flask(__name__, static_folder='./static', template_folder='./templates')
 
 users = {
     'smk1': '1234'
@@ -13,21 +13,42 @@ def login():
         username = request.form['username']
         password = request.form['password']
         if username in users and users[username] == password:
-            return redirect(url_for('subpage'))
+            return redirect(url_for('mainpage'))
         else:
             error = "아이디 또는 비밀번호가 올바르지 않습니다."
     return render_template('index.html', error=error)
 
 
 
-@app.route('/submenu/jumpGame')
+@app.route('/mainpage')
+def mainpage():
+    return render_template('mainpage.html')
+
+@app.route('/menu/game/jumpGame')
 def jumpGame():
-    return render_template('submenu/jumpGame.html')
+    return render_template('menu/game/jumpGame.html')
 
 
-@app.route('/subpage')
-def subpage():
-    return render_template('subpage.html')
+
+@app.route('/menu/game/tic_tac_toe')
+def tic_tac_toe():
+    return render_template('menu/game/tic_tac_toe.html')
+
+
+@app.route('/menu/gpt/gpt')
+def gpt():
+    return render_template('menu/gpt/gpt.html')
+
+
+
+
+@app.route('/menu/map/map')
+def map():
+    return render_template('menu/map/map.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
